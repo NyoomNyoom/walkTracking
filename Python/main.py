91a90a49421e9f909014a7d8dd42c17e7485e804
NyoomNyoom <jacksonnorth1275@gmail.com>
2025-02-05 17:48:01 +1300

    started working on the distance handler

--- Python/main.py
+++ Python/main.py
@@ -1,7 +1,12 @@
-goalDistance = 0
-walkedDistance = 0
+class DistanceCalculator:
+    def __init__(self, goalDist):
+        self.goalDistance = goalDist
+        self.walkedDistance = 0
 
-
+    def addDistance(self, numIn):
+        if numIn > 0:
+            self.walkedDistance += numIn
+    
 
 if __name__ == "__main__":
     print("running")
