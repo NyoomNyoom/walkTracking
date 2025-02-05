30e8dee62f5af2bd3ab81ac930a0aff6a719dc37
NyoomNyoom <jacksonnorth1275@gmail.com>
2025-02-05 18:07:52 +1300

    Added the ability to read in from a save file

--- Python/main.py
+++ Python/main.py
@@ -1,12 +1,27 @@
+import File
+
 class DistanceCalculator:
     def __init__(self, goalDist):
         self.goalDistance = goalDist
         self.walkedDistance = 0
 
+        try:
+            f = open("distanceWalked.txt", "r")
+            line = f.read()
+            self.walkedDistance = line
+            f.close()
+        except(FileNotFoundError):
+            self.walkedDistance = 0
+            f = open("distanceWalked", "w")
+            f.close()
+
     def addDistance(self, numIn):
         if numIn > 0:
             self.walkedDistance += numIn
-    
+
+class AppHandler:
+    def __init__(self):
+
 
 if __name__ == "__main__":
     print("running")
